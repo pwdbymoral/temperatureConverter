7192a45bd21db83a8899919e38b2e715d6614895
Vinícius Varjão <moraesvxp@gmail.com>
2024-04-02 19:47:02 -0300

    refactoring conversor

--- components/conversor/index.jsx
+++ components/conversor/index.jsx
@@ -1,24 +1,39 @@
-import { Text, TextInput, View, Button } from 'react-native';
+import { Text, TextInput, View, Pressable } from 'react-native';
 import React, { useState } from 'react';
 
 export default function Conversor() {
-  const [temperatura, onChangeTemperatura] = useState(0);
-  const [temperaturaConvertida, setTemperaturaConvertida] = useState(32);
+  const [temperaturaCelcius, setTemperaturaCelcius] = useState('0');
+  const [temperaturaFahrenheit, setTemperaturaFahrenheit] = useState(32);
 
   function converter() {
-    setTemperaturaConvertida(1.8 * temperatura + 32);
+    setTemperaturaFahrenheit(1.8 * temperaturaCelcius + 32);
   }
 
   return (
-    <View className="mx-auto w-1/2 flex-1 items-center justify-center space-y-4">
-      <Text className="w-full text-lg">Digite a temperatura em Celcius:</Text>
+    <View className="mx-auto w-2/3 flex-1 items-center justify-center space-y-4 rounded-xl bg-gray-100 px-2">
+      <Text className="w-full text-lg">Digite a temperatura em Celcius (ºC):</Text>
       <TextInput
-        className="mb-4 w-full bg-slate-100 px-2"
-        defaultValue="0"
-        onChangeText={(temperatura) => onChangeTemperatura(temperatura)}
+        className="w-full bg-slate-100 px-2"
+        keyboardType="numeric"
+        placeholder="Digite a temperatura em celcius"
+        defaultValue={temperaturaCelcius}
+        onChangeText={(temperaturaModificada) => {
+          setTemperaturaCelcius(temperaturaModificada);
+        }}
       />
-      <Button title="Converter" className="w-full" onPress={converter} />
-      <Text className="text-lg">Resultado: {temperaturaConvertida}</Text>
+      <Pressable
+        className="w-40 rounded-md bg-cyan-500"
+        onPress={() => {
+          converter();
+        }}
+      >
+        <Text className="p-2 text-lg">Converter</Text>
+      </Pressable>
+      <View className="w-2/3 bg-cyan-800 p-2 shadow-xl">
+        <Text className="text-lg text-gray-100">
+          Temperatura em Fahrenheit: {temperaturaFahrenheit}ºF
+        </Text>
+      </View>
     </View>
   );
 }
