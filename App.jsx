0f9b5fd78fcdf6108d263e3a6ece90c726f85a69
Vinícius Varjão <moraesvxp@gmail.com>
2024-04-02 19:48:06 -0300

    updating app interface

--- App.jsx
+++ App.jsx
@@ -5,10 +5,14 @@ import Conversor from './components/conversor';
 export default function App() {
   return (
     <View className="h-screen bg-gray-200">
-      <View className="flex flex-row items-center justify-between bg-blue-400 p-4">
-        <Text className="text-2xl font-bold">Conversor Bolado 3000</Text>
+      <View className="flex flex-row items-center justify-between bg-cyan-800 p-4">
+        <Text className="text-3xl font-bold text-slate-100">Conversor Bolado 3000</Text>
       </View>
-      <Conversor />
+      <View className="flex h-full items-center justify-center">
+        <View className="h-96">
+          <Conversor />
+        </View>
+      </View>
     </View>
   );
 }
